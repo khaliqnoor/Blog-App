90a73991cc9451ab6b400849b4df6e25cb83c609
Khaliq Noor <khaliqnoor369@gmail.com>
2025-11-27 02:31:14 -0800

    Initial commit from Create Next App

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      this is the page of the navbar and you should be aware of this
+    </div>
+  )
+}
+
+export default Navbar
